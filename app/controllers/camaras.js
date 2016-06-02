be47ca19c02d4a267614ebcc0c33beca65bf5de1
alu0100697414 <alu0100697414@ull.edu.es>
2016-06-02 18:33:44 +0100

    Funcionando el AES 256 CBC.

--- app/controllers/camaras.js
+++ app/controllers/camaras.js
@@ -6,11 +6,45 @@ var mongoose = require('mongoose'),
 var Camara = require('../../app/models/camara');
 var Historial = require('../../app/models/historial');
 var Utilities = require('./utilities');
+var crypto = require('crypto');
 
 var error = {'response' : 404};
 var error_400 = {'response' : 400};
 var error_400_1 = {'responseEO' : 400};
 var ok = {'response' : 201};
+
+var Encrypt;
+
+Encrypt = (function() {
+  var cipher_desencriptar, cipher_encriptar, decrypt, encrypt, iv, key;
+  key = crypto.createHash("sha256").update("somepassword").digest();
+  iv = '4e5Wa71fYoT7MFEX';
+  cipher_desencriptar = function(mode, data) {
+    var encipher, encoded;
+    encipher = crypto[mode]("aes-256-cbc", key, iv);
+    encoded = encipher.update(data,'base64','utf8');
+    encoded += encipher.final('utf8');
+    return encoded;
+  };
+  cipher_encriptar = function(mode, data) {
+    var encipher, encoded;
+    encipher = crypto[mode]("aes-256-cbc", key, iv);
+    encoded = encipher.update(data,'utf8','base64');
+    encoded += encipher.final('base64');
+    return encoded;
+  };
+  encrypt = function(data) {
+    return cipher_encriptar("createCipheriv", data);
+  };
+  decrypt = function(data) {
+    return cipher_desencriptar("createDecipheriv", data);
+  };
+  return {
+    encrypt: encrypt,
+    decrypt: decrypt
+  };
+})();
+
 
 /* Views Responce */
 exports.index = function (req, res) {
@@ -51,6 +85,12 @@ exports.getHistorial = function (request, response) {
     Historial.find({}, null, {sort: {time: -1}},function (err, historiales) {
         if (!err) {
             response.send(historiales);
+
+            // var src = 'me';
+            // var enc = Encrypt.encrypt(src);
+            // console.log('encrypted: ', enc);
+            // var dec = Encrypt.decrypt(enc);
+            // console.log('decrypted: ', dec);
         } else {
             console.log(err);
             response.send(error);
@@ -89,16 +129,26 @@ exports.delete = function (request, response) {
 /* New camera */
 exports.new = function (request, response) {
 
+    // var src = 'me';
+    // var enc = Encrypt.encrypt(src);
+    // console.log('encrypted: ', enc);
+    // var dec = Encrypt.decrypt(enc);
+    // // var dec = Encrypt.decrypt('rh7ro9NH');
+    // console.log('decrypted: ', dec);
+
     if ( Utilities.isEmpty(request.body.name)) return response.send(error_400);
     if ( Utilities.isEmpty(request.body.server)) return response.send(error_400);
 
-    Camara.find({name: request.body.name}).exec(function (err, camaras) {
+    var Dname = Encrypt.decrypt(request.body.name);
+    var Dserver = Encrypt.decrypt(request.body.server);
+
+    Camara.find({name: Dname}).exec(function (err, camaras) {
 
         if (err) return response.send(error);
         if (!Utilities.isEmpty(camaras)) return response.send(error);
 
-        var server = "rtmp://" + request.body.server + request.body.name;
-        var camaranueva = new Camara({ server: server, name: request.body.name});
+        var server = "rtmp://" + Dserver + Dname;
+        var camaranueva = new Camara({ server: server, name: Dname});
         camaranueva.save();
 
         response.send(ok);
@@ -125,22 +175,27 @@ exports.putonline = function (request, response) {
     if (Utilities.isEmpty(request.body.server)) return response.send(error_400);
     if (Utilities.isEmpty(request.body.time_now)) return response.send(error_400_);
 
+    var DTime_now = Encrypt.decrypt(request.body.time_now);
+    var DNombre = Encrypt.decrypt(request.body.nombre);
+    var DNumero = Encrypt.decrypt(request.body.numero);
+    var DServer = Encrypt.decrypt(request.body.server);
+
     Camara.find({name: request.params.name}).exec(function (err, camara) {
 
         if (err) response.send(error_400);
         if (Utilities.isEmpty(camara)) return response.send(error_400);
 
         camara[0].online = true;
-        camara[0].time_online = request.body.time_now;
-        camara[0].number = request.body.numero;
-        camara[0].nombre = request.body.nombre;
+        camara[0].time_online = DTime_now;
+        camara[0].number = DNumero;
+        camara[0].nombre = DNombre;
         camara[0].save();
 
-        Historial.find({name: request.body.name}).exec(function (err, historiales) {
+        Historial.find({name: request.params.name}).exec(function (err, historiales) {
             if (err) return response.send(error);
 
-            var server = "rtmp://" + request.body.server + request.body.name;
-            var historial_nuevo = new Historial({ name: request.body.name, nombre: request.body.nombre, numero: request.body.numero, time: request.body.time_now });
+            var server = "rtmp://" + DServer + request.params.name;
+            var historial_nuevo = new Historial({ name: request.params.name, nombre: DNombre, numero: DNumero, time: DTime_now });
             historial_nuevo.save();
 
             response.send(ok);
@@ -153,11 +208,13 @@ exports.putoffline = function (request, response) {
     if (Utilities.isEmpty(request.params.name)) return response.send(error_400);
     if (Utilities.isEmpty(request.body.date_last_online)) return response.send(error_400);
 
+    var DTime_now = Encrypt.decrypt(request.body.date_last_online);
+
     Camara.find({name: request.params.name}).exec(function (err, camara) {
         if (err) response.send(error_400);
         if (Utilities.isEmpty(camara)) return response.send(error_400);
         camara[0].online = false;
-        camara[0].time_offline = request.body.date_last_online;
+        camara[0].time_offline = DTime_now;
         camara[0].save();
         response.send(ok);
     });
